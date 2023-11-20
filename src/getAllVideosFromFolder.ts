7de1a0bbf5ba554823fbba9b624fe5d8d2678d4b
Ícaro Harry <icaro.harry@toptal.com>
2023-11-20 20:32:06 -0300

    Improve SQL generator

--- src/getAllVideosFromFolder.ts
+++ src/getAllVideosFromFolder.ts
@@ -1,5 +1,8 @@
-import axios from 'axios';
-import dotenv from 'dotenv';
+import axios from "axios";
+import dotenv from "dotenv";
+import fs from "fs";
+import csv from "csv-parser";
+import slugify from "slugify";
 
 dotenv.config();
 
@@ -7,13 +10,25 @@ dotenv.config();
 // Altere aqui!
 const VIMEO_TOKEN = process.env.VIMEO_TOKEN;
 const VIMEO_FOLDER_ID = process.env.VIMEO_FOLDER_ID;
+const WORKSHOP_ID = process.env.WORKSHOP_ID;
 // ======================
 
-if(!VIMEO_TOKEN) throw new Error('VIMEO_TOKEN not found in .env file');
-if(!VIMEO_FOLDER_ID) throw new Error('VIMEO_FOLDER_ID not found in .env file');
+if (!VIMEO_TOKEN) throw new Error("VIMEO_TOKEN not found in .env file");
+if (!VIMEO_FOLDER_ID) throw new Error("VIMEO_FOLDER_ID not found in .env file");
+const results: any[] = [];
 
-getAllVideosFromFolder(VIMEO_FOLDER_ID);
+readCSV(() => getAllVideosFromFolder(VIMEO_FOLDER_ID));
 
+function readCSV(callback: any) {
+  fs.createReadStream("data.csv")
+    .pipe(csv())
+    .on("data", (row) => {
+      results.push(row);
+    })
+    .on("end", () => {
+      callback();
+    });
+}
 
 function getAllVideosFromFolder(folderId: string | number) {
   axios
@@ -24,19 +39,33 @@ function getAllVideosFromFolder(folderId: string | number) {
     })
     .then((response) => {
       response.data.data.forEach((video: any) => {
-        console.log(
-          '"' +
-            video.name +
-            ',' +
-            video.link.replace('https://vimeo.com/', '') +
-            ',' +
-            video.duration +
-            '"'
-        );
+        if (!isNaN(video.name)) {
+          let index = Number(video.name) - 1;
+          results[index]["video_url"] = video.link.replace(
+            "https://vimeo.com/",
+            "https://player.vimeo.com/video/"
+          );
+          results[index]["duration_in_seconds"] = video.duration;
+          results[index]["slug"] = slugify(results[index].name, {
+            lower: true,
+          });
+        }
       });
-      // console.log(response.data);
+
+      generateSQL();
     })
     .catch((error) => {
       console.log(error);
     });
+}
+
+function generateSQL() {
+  results.map((result) => {
+    const sql =
+      `INSERT INTO "lessons" ("workshop_id", "name", "description", "video_url", "duration_in_seconds", "slug", "created_at", "updated_at") values (${WORKSHOP_ID}, '${result.name}', '${result.description}', '${result.video_url}', ${result.duration_in_seconds}, '${result.slug}', NOW(), NOW() );`.replaceAll(
+        '"',
+        "`"
+      );
+    console.log(sql);
+  });
 }
